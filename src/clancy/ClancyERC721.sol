1b7a9c2daf3c7feb5607f1b9b5e35103df894be6
Your Name <you@example.com>
2023-03-17 11:18:55 -0400

    Another commit actually

--- src/clancy/ClancyERC721.sol
+++ src/clancy/ClancyERC721.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+
 contract ClancyERC721 {
-    using Counters for Counters.Counter;
+    // using Counters for Counters.Counter;
 }

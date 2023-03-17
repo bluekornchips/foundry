01d7da8e966ee5105870f7122d7f54ad006d755a
some name <no@email.com>
2023-03-17 11:18:11 -0400

    Initial commit i think

---
+++ test/clancy/ClancyERC721.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "clancy/ClancyERC721.sol";
+
+contract ClancyERC721_Test is Test {
+    ClancyERC721 public clancyERC721;
+}

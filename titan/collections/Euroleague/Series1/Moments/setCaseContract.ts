c09a4b7c2b6426be96475301030e888c3e1c69bc
Your Name <you@example.com>
2023-03-31 10:52:05 -0400

    Added natspec

--- titan/collections/Euroleague/Series1/Moments/setCaseContract.ts
+++ titan/collections/Euroleague/Series1/Moments/setCaseContract.ts
@@ -1,7 +1,16 @@
 import { ethers } from "ethers"
 import Ducky from "../../../../utility/logging/ducky"
+
 const FILE_DIR = 'titan/collections/Euroleague/Series1/Moments'
 
+/**
+ * Sets the case contract address and its status on the moments contract.
+ * @param moments_contract The moments contract instance.
+ * @param case_contract_address The address of the case contract.
+ * @param status The status to set for the case contract.
+ * @returns A boolean indicating if the operation was successful.
+ * @throws An error if the transaction fails or if any other error occurs.
+ */
 const setCaseContract = async (moments_contract: ethers.Contract, case_contract_address: string, status: boolean): Promise<boolean> => {
     Ducky.Debug(FILE_DIR, "setCaseContract", `Setting case contract ${case_contract_address} to ${status} on moments contract ${await moments_contract.getAddress()}.`)
     try {
